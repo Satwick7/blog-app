3fa31ef74355e97e0e03655ee3bcbcbdb8b6ef30
Tuniki Satwick <satwick.tuniki11@gmail.com>
2024-03-26 20:13:06 +0530

    small fix app.js

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+dotenv.config();
+
 const app = express();
 
-const mongoURI = process.env('MONGO_URI');
-
+const mongoURI = process.env.MONGO_URI;
+console.log('entree :  ', mongoURI)
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
